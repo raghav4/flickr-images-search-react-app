abd317a21a3bd70a1357b20a0ca2a6b519685f14
Raghav <raghavsyt@gmail.com>
2021-05-08 19:52:17 +0530

    feat: updated loader

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,18 +1,22 @@
 /* eslint-disable no-console */
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
+import LinearProgress from '@material-ui/core/LinearProgress';
 import { CardGrid, SearchBox } from '../components';
 
 const Home = () => {
   const [images, setImages] = useState([]);
   const [loaded, setLoaded] = useState(true);
+  const [searchResultsTitle, setSearchResultsTitle] = useState('🆕 Showing Recent Images');
 
   const fetchImages = async (title = '') => {
+    setLoaded(true);
     const requestUrl = title ? `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=5adc0ef7be763da42ce94e17d0a3b3cf&text=${title}&page=20&format=json&nojsoncallback=1` : 'https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=5adc0ef7be763da42ce94e17d0a3b3cf&format=json&nojsoncallback=1';
+    setSearchResultsTitle(title ? `Showing search results for "${title}"` : '🆕 Showing Recent Images');
     try {
       const imagesData = await axios.get(requestUrl);
       const photos = imagesData.data.photos.photo;
-      console.log('Fetched Images...', photos);
+      // console.log('Fetched Images...', photos);
       setImages(photos);
       setLoaded(false);
     } catch (ex) {
@@ -28,14 +32,17 @@ const Home = () => {
     <>
       <SearchBox fetchCustomImages={fetchImages} />
       {loaded && (
-      <h3 style={{
-        textAlign: 'center', position: 'fixed', top: '50%', left: '33%',
-      }}
-      >
-        Please wait, images are being loaded...
-      </h3>
+      <>
+        <LinearProgress color="secondary" />
+        <h3 style={{
+          textAlign: 'center', position: 'fixed', top: '50%', left: '33%',
+        }}
+        >
+          Please wait, images are being loaded...
+        </h3>
+      </>
       )}
-      <h4 className="my-3" style={{ textAlign: 'center' }}>🆕 Showing Recent Images</h4>
+      <h4 className="my-3" style={{ textAlign: 'center' }}>{searchResultsTitle}</h4>
       <CardGrid images={images} />
     </>
   );
